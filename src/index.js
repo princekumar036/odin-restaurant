a35ba7c977ebd3c89449acff62197e2f879c7b9c
Prince Kumar <princekumar036@gmail.com>
2022-06-12 17:26:49 +0530

    fix explore menu btn

--- src/index.js
+++ src/index.js
@@ -3,34 +3,41 @@ import createHome from "./home";
 import createMenu from "./menu";
 import createContact from "./contact"
 
+const functionMapper = {
+    createHome: createHome,
+    createMenu: createMenu,
+    createContact: createContact
+}
+
+// Append Header and Main to body
 document.body.appendChild( createHeader() )
 const MAIN = document.createElement('main')
 document.body.appendChild(MAIN)
 
-MAIN.appendChild( createHome() )
-document.querySelector('nav li:nth-child(1)').classList.add('active')
-
-
+// Change Main content on Tab click
 const NAVS = document.querySelectorAll('li')
 NAVS.forEach(nav => {
     nav.addEventListener('pointerdown', (e) => {
-        MAIN.firstChild.remove()
-
-        if (e.target.innerText === 'Home') {
-            MAIN.appendChild( createHome() )
-            NAVS.forEach(nav => nav.classList.remove('active'))
-            document.querySelector('nav li:nth-child(1)').classList.add('active')
-        }
-
-        else if (e.target.innerText === 'Menu') {
-            MAIN.appendChild( createMenu() )
-            NAVS.forEach(nav => nav.classList.remove('active'))
-            document.querySelector('nav li:nth-child(2)').classList.add('active')
-        }
-        if (e.target.innerText === 'Contact') {
-            MAIN.appendChild( createContact() )
-            NAVS.forEach(nav => nav.classList.remove('active'))
-            document.querySelector('nav li:nth-child(3)').classList.add('active')
-        }
+        let tgt = e.target
+        let pos = [...tgt.parentElement.children].indexOf(tgt) + 1
+        changeMainContent(tgt.innerText, pos)
     })
 })
+
+function changeMainContent(tabName, pos) {
+    if (MAIN.firstChild) { MAIN.firstChild.remove() }
+    MAIN.appendChild( functionMapper[`create${tabName}`]() )
+    NAVS.forEach(nav => nav.classList.remove('active'))
+    document.querySelector(`nav li:nth-child(${pos})`).classList.add('active')
+}
+changeMainContent('Home', 1)
+
+// console.log(MAIN.firstChild.classList)
+// On Home Button click
+if (MAIN.firstChild.classList.contains('home')) {
+    console.log('OK')
+    const btn = document.querySelector('button')
+    btn.addEventListener('pointerdown', () => {
+        changeMainContent('Menu', 2)
+    })
+}
